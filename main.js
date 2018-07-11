d2d19d57b0fd3e0b48193788d0780f699354e481
mikeyim337 <mk337337@gmail.com>
2018-07-11 10:46:08 -0700

    move funtion finished upleft

---
+++ main.js
@@ -0,0 +1,24 @@
+// function movement(row, column) {
+//     if ()
+
+// }
+
+//get the value of row attribute and column attribute, respectively.
+var rowInitial = $(this).attr("row")
+var columnInitial = $(this).attr("column")
+
+function moveLeft(rowInitial, columnInitial) {
+    var rowFinal = rowInitial - 1;
+    var columnFinal = columnInitial - 1;
+    //removes red or black chip from initial position
+    $(this).removeClass("red").removeClass("black");
+    //adds red or black chip to final position
+    .attr()
+
+    if 
+}
+
+function moveRight(rowInitial, columnInitial) {
+    var rowFinal = rowInitial - 1;
+    var columnFinal = columnInitial + 1;
+}
