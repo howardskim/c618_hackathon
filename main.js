13aaa7e296a8f2279015aebdf5478d43c37b2845
mikeyim337 <mk337337@gmail.com>
2018-07-11 11:24:20 -0700

    kind of done with move function

--- main.js
+++ main.js
@@ -4,21 +4,65 @@
 // }
 
 //get the value of row attribute and column attribute, respectively.
+
+
+function upLeft(rowInitial, columnInitial) {
+
+
 var rowInitial = $(this).attr("row")
 var columnInitial = $(this).attr("column")
 
-function moveLeft(rowInitial, columnInitial) {
+
     var rowFinal = rowInitial - 1;
     var columnFinal = columnInitial - 1;
     //removes red or black chip from initial position
     $(this).removeClass("red").removeClass("black");
     //adds red or black chip to final position
-    .attr()
-
-    if 
+   var destination = "[row=rowFinal][column=columnFinal]"
+   if (turn%2 === 0) {
+    $(destination).addClass("red");
+   } else {
+     $(destination).addClass("black");  
+   }
+  
+    
 }
 
-function moveRight(rowInitial, columnInitial) {
+function upRight(rowInitial, columnInitial) {
     var rowFinal = rowInitial - 1;
     var columnFinal = columnInitial + 1;
+    $(this).removeClass("red").removeClass("black");
+    //adds red or black chip to final position
+   var destination = "[row=rowFinal][column=columnFinal]"
+   if (turn%2 === 0) {
+    $(destination).addClass("red");
+   } else {
+     $(destination).addClass("black");  
+   }
+}
+
+function downLeft(rowInitial, columnInitial) {
+    var rowFinal = rowInitial + 1;
+    var columnFinal = columnInitial -1;
+    $(this).removeClass("red").removeClass("black");
+    //adds red or black chip to final position
+    var destination = "[row=rowFinal][column=columnFinal]"
+    if (turn%2 === 0) {
+     $(destination).addClass("red");
+    } else {
+      $(destination).addClass("black");  
+    }
+}
+
+function downRight(rowInitial, columnInitial) {
+    var rowFinal = rowInitial + 1;
+    var columnFinal = columnInitial -1;
+    $(this).removeClass("red").removeClass("black");
+    //adds red or black chip to final position
+    var destination = "[row=rowFinal][column=columnFinal]"
+    if (turn%2 === 0) {
+     $(destination).addClass("red");
+    } else {
+      $(destination).addClass("black");  
+    }
 }
