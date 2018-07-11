fda65db9598923d8f3865aa00ebb9e8dd714539c
Matt Kirby <MattKir@aol.com>
2018-07-11 15:56:01 -0700

    Changed jump function to highlight where to jump

--- main.js
+++ main.js
@@ -58,61 +58,65 @@ function downRight(rowInitial, columnInitial) {
         $(destination).addClass("black");
     }
 }
-    function jumpUpLeft(posX, posY) {
-        var user;
-        var enemy;
-
-        if (gamePiecePos.hasClass("red")) {
-            return;
-        } else if (gamePiecePos.hasClass("black")) {
-            return;
+    function jumpUpLeft(row, column) {
+        var rowFinal = row - 2;
+        var columnFinal = column - 2;
+        //removes red or black chip from initial position
+        $(this).removeClass("red").removeClass("black");
+        //adds red or black chip to final position  
+    
+        var destination = `[row=${rowFinal}][column=${columnFinal}]`
+        if (turn % 2 === 0) {
+            $(destination).addClass("red");
+        } else {
+            $(destination).addClass("black");
         }
-        if (user.hasClass("red")) {
-            enemy.removeClass("black");
-            gamePiecePos[posX - 2][posY - 2];
-        } else return;
-        if (user.hasClass("black")) {
-            enemy.removeClass("red");
-            gamePiecePos[posX - 2][posY - 2];
-        } else return;
     }
 
-    function jumpUpRight(posX, posY) {
-        var user;
-        var enemy;
+    function jumpUpRight(row, column) {
+        var rowFinal = row - 2;
+        var columnFinal = column + 2;
+        //removes red or black chip from initial position
+        $(this).removeClass("red").removeClass("black");
+        //adds red or black chip to final position  
 
-        if (gamePiecePos.hasClass("red")) {
-            return;
-        } else if (gamePiecePos.hasClass("black")) {
-            return;
+        var destination = `[row=${rowFinal}][column=${columnFinal}]`
+        if (turn % 2 === 0) {
+            $(destination).addClass("red");
+        } else {
+            $(destination).addClass("black");
         }
-        if (user.hasClass("red")) {
-            enemy.removeClass("black");
-            gamePiecePos[posX - 2][posY + 2];
-        } else return;
-        if (user.hasClass("black")) {
-            enemy.removeClass("red");
-            gamePiecePos[posX - 2][posY + 2];
-        } else return;
     }
 
-    function jumpDownLeft(posX, posY) {
-        var user;
-        var enemy;
-
-        if (gamePiecePos.hasClass("red")) {
-            return;
-        } else if (gamePiecePos.hasClass("black")) {
-            return;
+    function jumpDownLeft(row, column) {
+        var rowFinal = row + 2;
+        var columnFinal = column - 2;
+        //removes red or black chip from initial position
+        $(this).removeClass("red").removeClass("black");
+        //adds red or black chip to final position  
+    
+        var destination = `[row=${rowFinal}][column=${columnFinal}]`
+        if (turn % 2 === 0) {
+            $(destination).addClass("red");
+        } else {
+            $(destination).addClass("black");
         }
-        if (user.hasClass("red")) {
-            enemy.removeClass("black");
-            gamePiecePos[posX + 2][posY - 2];
-        } else return;
-        if (user.hasClass("black")) {
-            enemy.removeClass("red");
-            gamePiecePos[posX + 2][posY - 2];
-        } else return;
+    }
+
+    function jumpDownRight(row, column) {
+        var rowFinal = row + 2;
+        var columnFinal = column + 2;
+        //removes red or black chip from initial position
+        $(this).removeClass("red").removeClass("black");
+        //adds red or black chip to final position  
+
+        var destination = `[row=${rowFinal}][column=${columnFinal}]`
+        if (turn % 2 === 0) {
+            $(destination).addClass("red");
+        } else {
+            $(destination).addClass("black");
+        }
+
     }
 
 function buildGameBoard() {
@@ -122,14 +126,14 @@ function buildGameBoard() {
     var boardGameArray =                            
      [
 
-      ["", "", "", "", "", "", "", "" ],
-      ["", "", "", "", "", "", "", "" ],
-      ["", "", "", "", "", "", "", "" ],
-      ["", "", "", "", "", "", "", "" ],  
-      ["", "", "", "", "", "", "", "" ],  
-      ["", "", "", "", "", "", "", "" ],  
-      ["", "", "", "", "", "", "", "" ],  
-      ["", "", "", "", "", "", "", "" ],  
+      ["", "", "", "", "", "", "", "", ],
+      ["", "", "", "", "", "", "", "", ],
+      ["", "", "", "", "", "", "", "", ],
+      ["", "", "", "", "", "", "", "", ],  
+      ["", "", "", "", "", "", "", "", ],  
+      ["", "", "", "", "", "", "", "", ],  
+      ["", "", "", "", "", "", "", "", ],  
+      ["", "", "", "", "", "", "", "", ],  
 
     ];
 
@@ -155,24 +159,7 @@ function buildGameBoard() {
             };
 
             row.append(column);
-
-    function jumpDownRight(posX, posY) {
-        var user;
-        var enemy;
-
-        if (gamePiecePos.hasClass("red")) {
-            return;
-        } else if (gamePiecePos.hasClass("black")) {
-            return;
-
         }
-        if (user.hasClass("red")) {
-            enemy.removeClass("black");
-            gamePiecePos[posX + 2][posY + 2];
-        } else return;
-        if (user.hasClass("black")) {
-            enemy.removeClass("red");
-            gamePiecePos[posX + 2][posY + 2];
-        } else return;
-
     }
+}
+
